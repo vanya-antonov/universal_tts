060e5d00650c323c5b1ae63bae69e301f6a2150f
Ivan Antonov <vanya.antonov@gmail.com>
2019-02-22 17:53:28 +0300

    Repeat subtypes Figure was added

--- images_R/repeat_subtypes.R
+++ images_R/repeat_subtypes.R
@@ -3,7 +3,6 @@ source("lib.R")
 
 ###
 
-REP_TYPES <- c("Low_complexity", "Simple_repeat", "LINE", "SINE", "LTR", "DNA", 'Other')
 REP_COLS <- c('chr', 'start', 'end', 'name', 'rep_chr', 'rep_start', 'rep_end', 'type_subtype', 'ovlp_len')
 
 read_repeat_data <- function(repeat_fn, uni_tts_fn, dna_group)
@@ -14,10 +13,9 @@ read_repeat_data <- function(repeat_fn, uni_tts_fn, dna_group)
   uni_tts_v <- read.delim(paste0(DATA_DIR, uni_tts_fn), as.is=TRUE, header=FALSE)$V4
   paste0(DATA_DIR, repeat_fn) %>%
     read.delim(as.is=TRUE, header=FALSE, col.names = REP_COLS) %>%
-    separate(type_subtype, sep = "::", into = c('rep_type', 'rep_subtype')) %>%
-    mutate(rep_type = if_else(rep_type %in% REP_TYPES, rep_type, 'Other')) %>%
-    # select(name, rep_type, ovlp_len) %>%
-    group_by(name, rep_type) %>%
+    separate(type_subtype, sep = "::", into = c('rep_type', 'rep_subtype'), remove = FALSE) %>%
+    filter(rep_type %in% c("Simple_repeat", "Low_complexity")) %>%
+    group_by(name, type_subtype, rep_type, rep_subtype) %>%
     summarise(ovlp_sum_len = sum(ovlp_len)) %>%
     # There must be at least 10 bp of the overlap inside the region
     filter(ovlp_sum_len > 10) %>%
@@ -25,8 +23,6 @@ read_repeat_data <- function(repeat_fn, uni_tts_fn, dna_group)
     mutate(dna_type = ifelse(name %in% uni_tts_v, 'uni', 'other')) %>%
     mutate(dna_group = dna_group,
            group_type = paste0(dna_group, '_', dna_type))
-    # spread(rep_type, ovlp_sum_len, fill = 0) %>%
-    # gather(PLOT_REP_TYPES, key='rep_type', value='ovlp_sum_len') %>%
 }
 
 chop_df <- read_repeat_data('chop_seq_repeats.txt', 'uni_tts_chop_seq_hg38.bed', 'chop')
@@ -45,11 +41,16 @@ total_v['bkg_other'] = total_v['bkg'] - total_v['bkg_uni']
 total_v['capture_other'] = total_v['capture'] - total_v['capture_uni']
 
 stats_df <- bind_rows(bkg_df, chop_df, capture_df) %>%
-  group_by(dna_group, dna_type, group_type, rep_type) %>%
+  group_by(dna_group, dna_type, group_type, type_subtype, rep_type, rep_subtype) %>%
   summarise(dna_with_rep = n_distinct(name)) %>%
   mutate(rep_freq = dna_with_rep / total_v[group_type]) %>%
   ungroup()
-as.data.frame(stats_df)
+
+frequent_subtypes <- stats_df %>%
+  filter(rep_freq >= 0.05) %>%
+  arrange(rep_type, -rep_freq) %>%
+  pull(type_subtype) %>%
+  unique()
 
 group_type_names <- c(
   'chop_uni' = 'Universal ChOP peaks ',
@@ -60,12 +61,17 @@ group_type_names <- c(
   'capture_other' = 'Other Capture peaks')
 
 stats_df %>%
+  # Select frequent subtypes only
+  filter(type_subtype %in% frequent_subtypes) %>%
+  # Add rows with zeros for missin repeat subtypes in some groups
+  spread(type_subtype, rep_freq, fill = 0) %>%
+  gather(frequent_subtypes, key='type_subtype', value='rep_freq') %>%
   # Define the order of bars in each repeat (group_type)
   mutate(group_type = factor(group_type, levels = group_type_names %>% names() %>% rev())) %>%
-  # Define the order of repeat types
-  mutate(rep_type = factor(rep_type, levels = rev(REP_TYPES))) %>%
+  # Define the order of repeat subtypes
+  mutate(type_subtype = factor(type_subtype, levels = rev(frequent_subtypes))) %>%
   ggplot() +
-  aes(x = rep_type, y = rep_freq, fill = group_type) +
+  aes(x = type_subtype, y = rep_freq, fill = group_type) +
   geom_bar(col = 'black', stat = 'identity', position = 'dodge', width = 0.8) +
   # Add the '%' sign to the Y-axis: https://stackoverflow.com/a/41098629/310453
   scale_y_continuous(labels = scales::percent_format()) +
@@ -83,6 +89,6 @@ stats_df %>%
   # About reverse = TRUE: https://stackoverflow.com/a/49448745/310453
   # About ncol = 2: https://stackoverflow.com/a/36087339/310453
   guides(fill = guide_legend(reverse = TRUE, ncol = 2))
-ggsave('repeat_types.pdf', path = OUT_DIR, width = 7, height = 7)
+ggsave('repeat_subtypes.pdf', path = OUT_DIR, width = 9, height = 11)
 
 
